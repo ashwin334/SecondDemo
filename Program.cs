54d91ba49b80ceb030e61eb0d4bf6db33ef9f263
ashwin333 <ashwinpatil832@gmail.com>
2022-10-08 18:01:39 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers");
+            int a =Convert.ToInt32(Console.ReadLine()); // if input is null,then it will assign 0
+            //int b =int.Parse(Console.Readline(); //if input is null,then shows error
+            int b =Convert.ToInt32(Console.ReadLine());
+            int sum = a + b;
+            Console.WriteLine(sum); 
+            int sub = a - b;
+            Console.WriteLine(sub);
+            int m = a * b;
+            Console.WriteLine(m);   
+            
+            int div = a / b;    
+            Console.WriteLine(div); 
+
+            int mod = a % b;    
+            Console.WriteLine(mod); 
+
+        }
+    }
+}
